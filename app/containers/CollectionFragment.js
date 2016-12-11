8c07f85317bf0d4599a2802bbfb687a378153a97
noprom <tyee.noprom@qq.com>
2016-12-11 14:07:01 +0800

    add DiscoveryFragment

---
+++ app/containers/CollectionFragment.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class CollectionFragment extends Component {
+  render() {
+    return(
+      <Text>dis</Text>
+    );
+  }
+}
